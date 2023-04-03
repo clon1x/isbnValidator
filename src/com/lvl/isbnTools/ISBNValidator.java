3a831d847b0f6d00df3dcdc573dadc5e9ecc946f
LVL <luis.vinelerma@yahoo.com>
2023-04-03 15:05:48 +0200

    Creada estructura inicial del proyecto

---
+++ src/com/lvl/isbnTools/ISBNValidator.java
@@ -0,0 +1,5 @@
+package com.lvl.isbnTools;
+
+public class ISBNValidator {
+	
+}
---
+++ test/com/lvl/isbnTools/ISBNValidatorTest.java
@@ -0,0 +1,14 @@
+package com.lvl.isbnTools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ISBNValidatorTest {
+
+	@Test
+	void should_ReturnTrue_When_ISBNIsValid() {
+		fail("Not yet implemented");
+	}
+
+}
