cce78ea6ff1602181d2517a2e3380ab4ecdf4f14
LVL <luis.vinelerma@yahoo.com>
2023-04-04 22:23:06 +0200

    Added more tests for valid and invalid ISBN-10 codes

--- src/test/java/com/lvl/isbnTools/ISBNValidatorTest.java
+++ src/test/java/com/lvl/isbnTools/ISBNValidatorTest.java
@@ -3,41 +3,45 @@ package com.lvl.isbnTools;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 class ISBNValidatorTest {
 
-	@ParameterizedTest(name = "isbn:{0}")
-	@CsvFileSource(resources = "/validIsbn10Codes.csv")
-	void should_ReturnTrue_When_ISBNIsValid(String isbnCandidate) {
-
-		// given
-		String isbn = isbnCandidate;
-		ISBNValidator validator = new ISBNValidator();
-
-		// when
-		boolean isValid = validator.isValidISBN(isbn);
-
-		// then
-		assertTrue(isValid);
-	}
-
-	@Test
-	void should_ReturnFalse_When_ISBNIsNotValid() {
-
-		// given
-		String isbn = "1719587214";
-		ISBNValidator validator = new ISBNValidator();
-
-		// when
-		boolean isValid = validator.isValidISBN(isbn);
-
-		// then
-		assertFalse(isValid);
-	}
+	@Nested
+	class IsValidISBNTests {
 	
+		@ParameterizedTest(name = "isbn:{0}")
+		@CsvFileSource(resources = "/validIsbn10Codes.csv")
+		void should_ReturnTrue_When_ISBNIsValid(String isbnCandidate) {
 	
+			// given
+			String isbn = isbnCandidate;
+			ISBNValidator validator = new ISBNValidator();
+	
+			// when
+			boolean isValid = validator.isValidISBN(isbn);
+	
+			// then
+			assertTrue(isValid);
+		}
+	
+		@ParameterizedTest(name = "isbn:{0}")
+		@CsvFileSource(resources = "/invalidIsbn10Codes.csv")
+		void should_ReturnFalse_When_ISBNIsNotValid() {
+	
+			// given
+			String isbn = "1719587214";
+			ISBNValidator validator = new ISBNValidator();
+	
+			// when
+			boolean isValid = validator.isValidISBN(isbn);
+	
+			// then
+			assertFalse(isValid);
+		}
+	
+	}
 	
 }
