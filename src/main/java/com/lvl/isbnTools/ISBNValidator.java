2e178203116b02673689fc46b258fffa5df69df4
LVL <luis.vinelerma@yahoo.com>
2023-04-05 10:37:00 +0200

    Corrected error while parsing char into integer

--- src/main/java/com/lvl/isbnTools/ISBNValidator.java
+++ src/main/java/com/lvl/isbnTools/ISBNValidator.java
@@ -11,7 +11,7 @@ public class ISBNValidator {
 		for (int i = 0; i < 10; i++) {
 			char c = isbn.charAt(i);
 			if (!Character.isDigit(c)) throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
-			total += (10-i)*(int) c;
+			total += (10-i)* Character.getNumericValue(c);
 		}
 		
 		return (total % 11 == 0);
