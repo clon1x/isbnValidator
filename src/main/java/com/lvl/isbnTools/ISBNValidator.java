7c07f0d0b869a1ece1cd694db9ff92c6ad7bab16
LVL <luis.vinelerma@yahoo.com>
2023-04-05 11:05:15 +0200

    Accept ISBN-10 codes with an X

--- src/main/java/com/lvl/isbnTools/ISBNValidator.java
+++ src/main/java/com/lvl/isbnTools/ISBNValidator.java
@@ -7,14 +7,23 @@ public class ISBNValidator {
 		if (isbn.length() != 10) throw new NumberFormatException(Messages.getString("ISBNValidator.0")); //$NON-NLS-1$
 		
 		int total = 0;
+		isbn = isbn.toUpperCase(); 
 		
 		for (int i = 0; i < 10; i++) {
 			char c = isbn.charAt(i);
-			if (!Character.isDigit(c)) throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
-			total += (10-i)* Character.getNumericValue(c);
+			if (!Character.isDigit(c)) {
+				total += (10-i) * getValue(c);
+			} else {
+				total += (10-i) * Character.getNumericValue(c);
+			}
 		}
 		
 		return (total % 11 == 0);
+	}
+
+	private int getValue(char c) {
+		if (c == 'X') return 10;
+		throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
 	}
 	
 }
