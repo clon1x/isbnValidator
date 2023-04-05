9d07530b2602cc83c05ad7d9d8f6f4518a3c7e95
LVL <luis.vinelerma@yahoo.com>
2023-04-05 11:36:43 +0200

    Only accept 'X' as final character in ISBN-10 codes
    Hasta aquí he ido haciendo commit que añaden tests que fallan, para que
    se vea el proceso, pero a Develop sólo debería subirse código que pase
    todos los tests.
    
    A partir de ahora los commit irán con el Test + el código que hace que
    el test pase.

--- src/main/java/com/lvl/isbnTools/ISBNValidator.java
+++ src/main/java/com/lvl/isbnTools/ISBNValidator.java
@@ -11,19 +11,15 @@ public class ISBNValidator {
 		
 		for (int i = 0; i < 10; i++) {
 			char c = isbn.charAt(i);
-			if (!Character.isDigit(c)) {
-				total += (10-i) * getValue(c);
-			} else {
+			if (Character.isDigit(c)) {
 				total += (10-i) * Character.getNumericValue(c);
+			} else if (c == 'X' && i == 9){
+				total += 10;
+			} else {
+				throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
 			}
 		}
-		
+
 		return (total % 11 == 0);
 	}
-
-	private int getValue(char c) {
-		if (c == 'X') return 10;
-		throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
-	}
-	
 }
