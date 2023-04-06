024fca81aaaf7fd9a0130796a0e55b405a1dcd3b
LVL <luis.vinelerma@yahoo.com>
2023-04-06 13:42:58 +0200

    New business requirement: locator code
    This code is made from ISBN, author name and number of pages.
    It is an internal unique code used to keep control of the location of
    a particular book.
    
    To be able to create the code we'll use an external service to get the
    data for a book given it's ISBN.
    We'll cache the data in a database for reliability and performance. If
    a code is not in the database, then te service will be queried.

---
+++ src/test/java/com/lvl/isbnTools/StockManagerTest.java
@@ -0,0 +1,14 @@
+package com.lvl.isbnTools;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class StockManagerTest {
+
+	@Test
+	void should_ReturnCorrectLocatorCode_When_RecordIsInDatabase() {
+		fail("Not implemented");
+	}
+
+}
