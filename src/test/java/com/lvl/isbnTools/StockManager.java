6f03b95cfc911987bbff8ec6c0b57043388bcf94
LVL <luis.vinelerma@yahoo.com>
2023-04-06 14:21:51 +0200

    Get failing test for correct locator code
    Test implementation, no modifications in code.

---
+++ src/test/java/com/lvl/isbnTools/StockManager.java
@@ -0,0 +1,10 @@
+package com.lvl.isbnTools;
+
+public class StockManager {
+
+	public String getLocatorCode(String isbn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/test/java/com/lvl/isbnTools/StockManagerTest.java
+++ src/test/java/com/lvl/isbnTools/StockManagerTest.java
@@ -1,14 +1,25 @@
 package com.lvl.isbnTools;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class StockManagerTest {
 
 	@Test
-	void should_ReturnCorrectLocatorCode_When_RecordIsInDatabase() {
-		fail("Not implemented");
+	void should_ReturnCorrectLocatorCode_When_ValidISBN() {
+		
+		// given
+		StockManager db = new StockManager();
+		String isbn = "012000030X";
+		String expected = "030XC5";
+		
+		// when
+		String locatorCode = db.getLocatorCode(isbn);
+		
+		// then
+		assertEquals(expected, locatorCode);
+		
 	}
 
 }
