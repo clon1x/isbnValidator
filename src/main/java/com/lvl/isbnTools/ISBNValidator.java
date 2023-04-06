250ef42a7c7292aa175898a932f47e877b62605f
LVL <luis.vinelerma@yahoo.com>
2023-04-06 12:06:36 +0200

    Extract constants refactoring

--- src/main/java/com/lvl/isbnTools/ISBNValidator.java
+++ src/main/java/com/lvl/isbnTools/ISBNValidator.java
@@ -2,10 +2,16 @@ package com.lvl.isbnTools;
 
 public class ISBNValidator {
 
+	private static final int ISBN_10_LENGTH = 10;
+	private static final int ISBN_10_DIVISOR = 11;
+	private static final int ISBN_10_X_VALUE = 10;
+	private static final int ISBN_13_LENGTH = 13;
+	private static final int ISBN_13_DIVISOR = 10;
+
 	public boolean isValidISBN(String isbn) {
-		if (isbn.length() == 10)
+		if (isbn.length() == ISBN_10_LENGTH)
 			return isValidISBN10(isbn);
-		if (isbn.length() == 13)
+		if (isbn.length() == ISBN_13_LENGTH)
 			return isValidISBN13(isbn);
 		
 		throw new NumberFormatException(Messages.getString("ISBNValidator.0")); //$NON-NLS-1$
@@ -16,25 +22,25 @@ public class ISBNValidator {
 		int total = 0;
 		isbn = isbn.toUpperCase(); 
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < ISBN_10_LENGTH; i++) {
 			char c = isbn.charAt(i);
 			if (Character.isDigit(c)) {
-				total += (10-i) * Character.getNumericValue(c);
+				total += (ISBN_10_LENGTH-i) * Character.getNumericValue(c);
 			} else if (c == 'X' && i == 9){
-				total += 10;
+				total += ISBN_10_X_VALUE;
 			} else {
 				throw new NumberFormatException(Messages.getString("ISBNValidator.1")); //$NON-NLS-1$
 			}
 		}
 
-		return (total % 11 == 0);
+		return (total % ISBN_10_DIVISOR == 0);
 	}
 
 	private boolean isValidISBN13(String isbn) {
 		
 		int total = 0, digit = 0;
 		
-		for (int i = 0; i < 13; i++) {
+		for (int i = 0; i < ISBN_13_LENGTH; i++) {
 			char c = isbn.charAt(i);
 			if (!Character.isDigit(c)) throw new NumberFormatException(Messages.getString("ISBNValidator.2")); //$NON-NLS-1$
 			
@@ -42,7 +48,7 @@ public class ISBNValidator {
 			total += digit * (1 + 2 * (i % 2));
 		}
 		
-		return (total % 10 == 0);
+		return (total % ISBN_13_DIVISOR == 0);
 	}
 
 }
