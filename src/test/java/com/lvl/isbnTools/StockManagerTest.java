ebae6226ce0d87e30453ad656e1a4a92a918ad31
LVL <luis.vinelerma@yahoo.com>
2023-04-07 13:00:06 +0200

    Implement test should_UseDatabaseVersion_When_ISBNInDatabase
    Implemented using mocks for database and web services.
    Database service is configured to return a valid book.
    Web service is unconfigured, and so it would return a book with all fields equal to null.

--- src/test/java/com/lvl/isbnTools/StockManagerTest.java
+++ src/test/java/com/lvl/isbnTools/StockManagerTest.java
@@ -4,33 +4,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import com.lvl.isbnTools.model.Book;
 import com.lvl.isbnTools.service.BookDatabaseStub;
 import com.lvl.isbnTools.service.BookWebServiceDataStub;
+import com.lvl.isbnTools.service.ISBNDataService;
 
 class StockManagerTest {
 
 	@Test
 	void should_ReturnCorrectLocatorCode_When_ValidISBN() {
-		
+
 		// given
 		StockManager db = new StockManager(new BookWebServiceDataStub(), new BookDatabaseStub());
 		String isbn = "012000030X";
 		String expected = "030XC5";
-		
+
 		// when
 		String locatorCode = db.getLocatorCode(isbn);
-		
+
 		// then
 		assertEquals(expected, locatorCode);
-		
+
 	}
-	
+
 	@Test
 	void should_UseDatabaseVersion_When_ISBNInDatabase() {
-		fail("Not implemented");
+
+		// given
+		String isbn = "012000030X";
+		String expected = "030XC5";
+		ISBNDataService databaseServiceMock = Mockito.mock(ISBNDataService.class);
+		ISBNDataService webServiceMock = Mockito.mock(ISBNDataService.class);
+		StockManager manager = new StockManager(webServiceMock, databaseServiceMock);
+
+		Mockito.when(databaseServiceMock.lookup("012000030X"))
+				.thenReturn(new Book("012000030X", "The Last of the Mohicans", "Cooper, James Fenimore"));
+
+		// when
+		String locatorCode = manager.getLocatorCode(isbn);
+
+		// then
+		assertEquals(expected, locatorCode);
+
 	}
-	
+
 	@Test
 	void should_UseCloudVersion_When_ISBNNotInDatabase() {
 		fail("Not implemented");
