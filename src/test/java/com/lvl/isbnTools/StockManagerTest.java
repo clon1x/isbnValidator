0d08bc1ea159186911ef769be7bdbbe80fb656db
LVL <luis.vinelerma@yahoo.com>
2023-04-07 12:19:07 +0200

    Added 2 tests for StockService
    We will be checking Behaviour, not just data.
    We want to check whether the cloud isbn lookup service gets called or not.

--- src/test/java/com/lvl/isbnTools/StockManagerTest.java
+++ src/test/java/com/lvl/isbnTools/StockManagerTest.java
@@ -1,6 +1,7 @@
 package com.lvl.isbnTools;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +24,16 @@ class StockManagerTest {
 		// then
 		assertEquals(expected, locatorCode);
 		
+	}
+	
+	@Test
+	void should_UseDatabaseVersion_When_ISBNInDatabase() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	void should_UseCloudVersion_When_ISBNNotInDatabase() {
+		fail("Not implemented");
 	}
 
 }
